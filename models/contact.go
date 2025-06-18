26a8ea06c98dd396e4b1dd1e5a8ec352b3111bf9
Gandharv Talikoti <gandharvwork@gmail.com>
2025-06-18 19:01:51 +0530

    fix: replaced enum with string + constraint in link_precedence field

--- models/contact.go
+++ models/contact.go
@@ -6,11 +6,11 @@ import (
 
 type Contact struct {
 	ID             uint       `gorm:"primaryKey"`
-	PhoneNumber    *string    `gorm:"uniqueIndex"`
-	Email          *string    `gorm:"uniqueIndex"`
-	LinkedID       *uint
-	LinkPrecedence string     `gorm:"type:enum('primary','secondary')"`
-	CreatedAt      time.Time
-	UpdatedAt      time.Time
-	DeletedAt      *time.Time `gorm:"index"`
+	PhoneNumber    string     `gorm:"column:phone_number"`
+	Email          string     `gorm:"column:email"`
+	LinkedID       *uint      `gorm:"column:linked_id"` // Nullable
+	LinkPrecedence string     `gorm:"column:link_precedence;type:varchar(10);check:link_precedence IN ('primary','secondary')"`
+	CreatedAt      time.Time  `gorm:"column:created_at"`
+	UpdatedAt      time.Time  `gorm:"column:updated_at"`
+	DeletedAt      *time.Time `gorm:"column:deleted_at"` // Nullable
 }
